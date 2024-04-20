b55a140d4f44077e276f247fb98c469d4c2a14b7
Бакир Базаркул <bakirbazarkul16@gmail.com>
2024-04-20 19:54:46 +0300

    + telegram.php file2

--- public/telegram.php
+++ public/telegram.php
@@ -1,29 +1,29 @@
 <?php
-
-/* https://api.telegram.org/bot6313225012:AAF-lWBd2E42ZMS6Um2-l6Y2ynLIOPx1EAI/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
-
-$name = $_POST['user_name'];
-$phone = $_POST['user_phone'];
-$email = $_POST['user_email'];
 $token = "6313225012:AAF-lWBd2E42ZMS6Um2-l6Y2ynLIOPx1EAI";
 $chat_id = "-411094732";
-$arr = array(
-  'Имя пользователя: ' => $name,
-  'Телефон: ' => $phone,
-  'Email' => $email
-);
 
-// foreach($arr as $key => $value) {
-//   $txt .= "<b>".$key."</b> ".$value."%0A";
-// };
 $test_telgram_masege ='тестовый  отправка сообшения '
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$test_telgram_masege}","r");
 
 if ($sendToTelegram) {
-  header('Location: thank-you.html');
+ echo " gooot";
 } else {
   echo "Error";
 }
+?>
+
+<?php
+
+/* https://api.telegram.org/bot6313225012:AAF-lWBd2E42ZMS6Um2-l6Y2ynLIOPx1EAI/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+// $name = $_POST['user_name'];
+// $phone = $_POST['user_phone'];
+// $email = $_POST['user_email'];
+// $arr = array(
+//   'Имя пользователя: ' => $name,
+//   'Телефон: ' => $phone,
+//   'Email' => $email
+// );
 ?>
